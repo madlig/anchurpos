c3c75d5e56156d1eb92d13bf011e5195256f2ead
madlig <madlighifari29@gmail.com>
2026-08-02 17:32:10 +0700

    chore(release): cleanup test routes, ignore scratch files, and prepare production release

--- clean_master.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let code = fs.readFileSync('c:/mad/website/anchurpos/app/manager/employees/master/page.tsx', 'utf8');
-code = code.replace(/<div className="flex bg-slate-100.*?<\/div>/s, '');
-code = code.replace(/\{tab === "absensi"[\s\S]*?(?=\{tab === "payroll")/s, '');
-code = code.replace(/\{tab === "payroll"[\s\S]*?(?=<\/div>\n\s*<\/div>\n\s*\);\n\})/s, '');
-code = code.replace(/tab === "karyawan" && \(/g, '');
-code = code.replace(/setTab={setTab}/g, '');
-fs.writeFileSync('c:/mad/website/anchurpos/app/manager/employees/master/page.tsx', code);
